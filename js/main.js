8b6d76212d19443c14f075fa87e94ba5adee5e5c
Guy Clark <guy@guy-MS-7816>
2016-02-11 23:45:14 +0000

    added comments to the javascript

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@ var myHeading = document.querySelector('h1');
 
 function setUserName() {
   var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Hello there, ' + myName;
+  localStorage.setItem('name', myName); //localstorage is used to store data inside your browser. In this case I am storing a data item called "name" and giving it the value of myName
+  myHeading.innerHTML = 'Hello there, ' + myName; // Edit the existing h1 tag HTML to say the users name
 }
 
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
+if(!localStorage.getItem('name')) { //Check to see if the data item "name exists"
+  setUserName(); // If it doesn't then run the function setUserName
+} else { //Else just set the name to what ever the data item "name" is.
   var storedName = localStorage.getItem('name');
   myHeading.innerHTML = 'Hello there, ' + storedName;
 }
 
-myButton.onclick = function() {
+myButton.onclick = function() { //Function to set the button 
   setUserName();
 }
