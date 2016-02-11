4ada29306029d59a7ef56975d10ce06307ecbe68
Guy Clark <guy@guy-MS-7816>
2016-02-11 23:36:25 +0000

    Changed the text of the header

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-var myButton = document.querySelector('button');
+var myButton = document.querySelector('button'); //querySelector is used to find a certian HTML tag
 var myHeading = document.querySelector('h1');
 
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = 'Hello there, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'Hello there, ' + storedName;
 }
 
 myButton.onclick = function() {
