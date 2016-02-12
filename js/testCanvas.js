d0843d2e8554ddf50d85a9dad170c4503de2bf0a
Guy Clark <guy@guy-MS-7816>
2016-02-12 00:00:54 +0000

    Adding canvas files

---
+++ js/testCanvas.js
@@ -0,0 +1,8 @@
+var c = document.getElementById(myCanvas);
+var ctx = c.getContent("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(0,0,200,100);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(450,450,200,100);
